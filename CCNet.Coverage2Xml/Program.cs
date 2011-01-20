f9b96f6c8dd0afe6dfd24490ec5f07a5a83deaaf
Pavel <pavel.mephi@gmail.com>
2011-01-20 18:00:29 +0300

    Now Coverage2Xml works with named arguments.

--- CCNet.Coverage2Xml/Program.cs
+++ CCNet.Coverage2Xml/Program.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Xsl;
+using CCNet.Common;
+using CCNet.Coverage2Xml.Properties;
 using Microsoft.VisualStudio.Coverage.Analysis;
 
 namespace CCNet.Coverage2Xml
@@ -12,22 +14,27 @@ namespace CCNet.Coverage2Xml
 	{
 		public static int Main(string[] args)
 		{
+			if (args == null || args.Length == 0)
+			{
+				DisplayUsage();
+				return 0;
+			}
+
 			try
 			{
-				string file = args[0];
-				string output = args[1];
+				Arguments.Default = ArgumentProperties.Parse(args);
 
-				if (!File.Exists(file))
+				if (!File.Exists(Arguments.DataCoverageFile))
 					return 0;
 
-				using (CoverageInfo info = CoverageInfo.CreateFromFile(file))
+				using (CoverageInfo info = CoverageInfo.CreateFromFile(Arguments.DataCoverageFile))
 				{
 					CoverageDS data = info.BuildDataSet();
 
 					string outputXml = Transform(data.GetXml());
 
 					XDocument doc = XDocument.Parse(outputXml);
-					File.WriteAllText(output, doc.ToString());
+					File.WriteAllText(Arguments.XmlCoverageFile, doc.ToString());
 				}
 			}
 			catch (Exception ex)
@@ -39,6 +46,19 @@ namespace CCNet.Coverage2Xml
 			return 0;
 		}
 
+		/// <summary>
+		/// Displays usage text.
+		/// </summary>
+		private static void DisplayUsage()
+		{
+			Console.WriteLine();
+			Console.WriteLine(Resources.UsageInfo);
+			Console.WriteLine();
+		}
+		
+		/// <summary>
+		/// Deletes unnecessary sections.
+		/// </summary>
 		private static string Transform(string xml)
 		{
 			XslCompiledTransform xslt = new XslCompiledTransform();
