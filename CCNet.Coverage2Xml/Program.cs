73fbf8b501ee44db031dd0741f1d8c4ded9dbe6f
Pavel <pavel.mephi@gmail.com>
2011-01-20 17:36:33 +0300

    New project CCNet.Coverage2Xml was added. Now it does not have named arguments.

---
+++ CCNet.Coverage2Xml/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Xsl;
+using Microsoft.VisualStudio.Coverage.Analysis;
+
+namespace CCNet.Coverage2Xml
+{
+	public class Program
+	{
+		public static int Main(string[] args)
+		{
+			try
+			{
+				string file = args[0];
+				string output = args[1];
+
+				if (!File.Exists(file))
+					return 0;
+
+				using (CoverageInfo info = CoverageInfo.CreateFromFile(file))
+				{
+					CoverageDS data = info.BuildDataSet();
+
+					string outputXml = Transform(data.GetXml());
+
+					XDocument doc = XDocument.Parse(outputXml);
+					File.WriteAllText(output, doc.ToString());
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.Error.WriteLine(ex.ToString());
+				return 1;
+			}
+
+			return 0;
+		}
+
+		private static string Transform(string xml)
+		{
+			XslCompiledTransform xslt = new XslCompiledTransform();
+			using (XmlTextReader xtr = new XmlTextReader(new StringReader(Xslt.Coverage)))
+			{
+				xslt.Load(xtr);
+			}
+
+			StringBuilder resultXml = new StringBuilder();
+			using (XmlTextReader xtr = new XmlTextReader(new StringReader(xml)))
+			{
+				using (StringWriter sw = new StringWriter(resultXml))
+				{
+					XsltArgumentList args = new XsltArgumentList();
+					xslt.Transform(xtr, args, sw);
+				}
+			}
+
+			return resultXml.ToString();
+		}
+	}
+}
