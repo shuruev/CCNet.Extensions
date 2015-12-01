f62b14e26ed8f548bc36861f7dc0e4cfe6052d3f
Oleg Shuruev <shuruev@gmail.com>
2015-11-30 16:43:17 -0800

    Only changed files are deployed

--- Deploy.bat
+++ Deploy.bat
@@ -3,11 +3,11 @@ SETLOCAL
 
 SET ccnetPath=\\rufc-devbuild.cneu.cnwk\d$\CruiseControl.NET
 
-XCOPY "CCNet.Extensions.Plugin\bin\Debug\CCNet.Extensions.Plugin.dll" "%ccnetPath%\CCValidator" /Y
-XCOPY "CCNet.Extensions.Plugin\bin\Debug\CCNet.Extensions.Plugin.dll" "%ccnetPath%\Server-Library" /Y
+XCOPY "CCNet.Extensions.Plugin\bin\Debug\CCNet.Extensions.Plugin.dll" "%ccnetPath%\CCValidator" /D /Y
+XCOPY "CCNet.Extensions.Plugin\bin\Debug\CCNet.Extensions.Plugin.dll" "%ccnetPath%\Server-Library" /D /Y
 
-XCOPY "CCNet.Build.Common\bin\Debug\Lean.Configuration.dll" "%ccnetPath%\CCNet.Build" /Y
-XCOPY "CCNet.Build.Common\bin\Debug\CCNet.Build.Common.dll" "%ccnetPath%\CCNet.Build" /Y
-XCOPY "CCNet.Build.GenerateNuspec\bin\Debug\CCNet.Build.GenerateNuspec.exe" "%ccnetPath%\CCNet.Build" /Y
+XCOPY "CCNet.Build.Common\bin\Debug\Lean.Configuration.dll" "%ccnetPath%\CCNet.Build" /D /Y
+XCOPY "CCNet.Build.Common\bin\Debug\CCNet.Build.Common.dll" "%ccnetPath%\CCNet.Build" /D /Y
+XCOPY "CCNet.Build.GenerateNuspec\bin\Debug\CCNet.Build.GenerateNuspec.exe" "%ccnetPath%\CCNet.Build" /D /Y
 
 PAUSE
