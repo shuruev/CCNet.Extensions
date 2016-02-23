620c235b894c9a241cb3ca7c63691b7fd9e3dc23
Oleg Shuruev <shuruev@gmail.com>
2016-02-23 10:55:54 -0800

    Added more extensions

---
+++ CCNet.Build.Common/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace CCNet.Build.Common
+{
+	/// <summary>
+	/// Some extension methods.
+	/// </summary>
+	public static class Extensions
+	{
+		/// <summary>
+		/// Gets detailed date/time description in two time zones.
+		/// </summary>
+		public static string ToDetailedString(this DateTime dateTime)
+		{
+			return String.Format(
+				CultureInfo.InvariantCulture,
+				"{0:yyyy-MM-dd}   {1:HH:mm:ss} MSK   {2:HH:mm:ss} PST",
+				dateTime.ToUniversalTime(),
+				TimeZoneInfo.ConvertTimeBySystemTimeZoneId(dateTime, "Russian Standard Time"),
+				TimeZoneInfo.ConvertTimeBySystemTimeZoneId(dateTime, "Pacific Standard Time"));
+		}
+
+		/// <summary>
+		/// Converts version to a form where all four components are specified.
+		/// </summary>
+		public static Version Normalize(this Version version)
+		{
+			if (version == null)
+				return null;
+
+			if (version.Build >= 0 && version.Revision >= 0)
+				return version;
+
+			return new Version(
+				version.Major,
+				version.Minor,
+				Math.Max(version.Build, 0),
+				Math.Max(version.Revision, 0));
+		}
+	}
+}
