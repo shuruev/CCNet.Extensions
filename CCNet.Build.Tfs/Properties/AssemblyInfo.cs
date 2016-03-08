42044a94a69d5093f616df372c1226000ca239f4
Oleg Shuruev <shuruev@gmail.com>
2016-03-07 16:34:57 -0800

    Added TFS client

---
+++ CCNet.Build.Tfs/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CCNet.Build.Tfs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Oleg Shuruev")]
+[assembly: AssemblyProduct("CCNet.Build.Tfs")]
+[assembly: AssemblyCopyright("Copyright © Oleg Shuruev 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9478f382-7fba-4e93-a7b3-1fe439ea388a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CCNet.Build.Tfs/TfsClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+
+namespace CCNet.Build.Tfs
+{
+	public class TfsClient
+	{
+		private readonly VersionControlServer m_server;
+
+		public TfsClient(string url)
+		{
+			if (String.IsNullOrEmpty(url))
+				throw new ArgumentNullException("url");
+
+			var credentials = new TfsClientCredentials();
+			var collection = new TfsTeamProjectCollection(new Uri(url), credentials);
+
+			m_server = collection.GetService<VersionControlServer>();
+		}
+
+		public string ReadFile(string path)
+		{
+			var item = m_server.GetItem(path);
+			using (var sr = new StreamReader(item.DownloadFile(), Encoding.UTF8))
+			{
+				return sr.ReadToEnd();
+			}
+		}
+	}
+}
