0216957c2dd30b7fd51440361f7105f24085876a
Oleg Shuruev <shuruev@gmail.com>
2016-04-07 18:42:06 -0700

    Improved checks for platforms and configurations

--- CCNet.Build.CheckProject/Issues/FileContents/CheckPrimarySolution.cs
+++ CCNet.Build.CheckProject/Issues/FileContents/CheckPrimarySolution.cs
@@ -6,18 +6,37 @@ namespace CCNet.Build.CheckProject
 {
 	public class CheckPrimarySolution : IChecker
 	{
+		private List<string> m_lines;
+		private Dictionary<string, string> m_projects;
+
 		public void Check(CheckContext context)
 		{
-			var file = context.TfsSolutionFile.Result;
-			var lines = file.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+			m_lines = ParseLines(context);
+			m_projects = ParseProjects(m_lines);
 
-			CheckVisualStudioVersion(lines);
-			CheckTeamFoundationServer(lines);
+			CheckVisualStudioVersion();
+			CheckTeamFoundationServer();
+			CheckSolutionConfigurationPlatforms();
+			CheckProjectConfigurationPlatforms();
 		}
 
-		private void CheckVisualStudioVersion(IList<string> lines)
+		private static List<string> ParseLines(CheckContext context)
 		{
-			if (IsVs2013(lines) || IsVs2015(lines))
+			var file = context.TfsSolutionFile.Result;
+			return file.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+		}
+
+		private static Dictionary<string, string> ParseProjects(IEnumerable<string> lines)
+		{
+			return lines
+				.Where(line => line.StartsWith("Project(\""))
+				.Select(line => line.Split(' '))
+				.ToDictionary(parts => parts[4].Trim('"'), parts => parts[2].Trim('"', ','));
+		}
+
+		private void CheckVisualStudioVersion()
+		{
+			if (IsVs2013() || IsVs2015())
 			{
 				return;
 			}
@@ -26,28 +45,108 @@ namespace CCNet.Build.CheckProject
 Please make sure solution file is saved with Visual Studio 2013 or above, so the others could work with it conveniently.");
 		}
 
-		private bool IsVs2013(IList<string> lines)
+		private bool IsVs2013()
 		{
-			return lines[0].StartsWith("Microsoft Visual Studio Solution File, Format Version 12.")
-				&& lines[1] == "# Visual Studio 2013"
-				&& lines[2].StartsWith("VisualStudioVersion = 12.");
+			return m_lines[0].StartsWith("Microsoft Visual Studio Solution File, Format Version 12.")
+				&& m_lines[1] == "# Visual Studio 2013"
+				&& m_lines[2].StartsWith("VisualStudioVersion = 12.");
 		}
 
-		private bool IsVs2015(IList<string> lines)
+		private bool IsVs2015()
 		{
-			return lines[0].StartsWith("Microsoft Visual Studio Solution File, Format Version 12.")
-				&& lines[1] == "# Visual Studio 14"
-				&& lines[2].StartsWith("VisualStudioVersion = 14.");
+			return m_lines[0].StartsWith("Microsoft Visual Studio Solution File, Format Version 12.")
+				&& m_lines[1] == "# Visual Studio 14"
+				&& m_lines[2].StartsWith("VisualStudioVersion = 14.");
 		}
 
-		private void CheckTeamFoundationServer(IEnumerable<string> lines)
+		private void CheckTeamFoundationServer()
 		{
-			var tfs = lines.Where(l => l.Contains("SccTeamFoundationServer")).ToList();
+			var tfs = m_lines.Where(l => l.Contains("SccTeamFoundationServer")).ToList();
 			if (tfs.Count == 1 && tfs[0] == "\t\tSccTeamFoundationServer = http://rufc-devbuild.cneu.cnwk:8080/tfs/sed")
 				return;
 
 			throw new FailedCheckException(@"Something looks wrong with source control configuration in solution file.
 Please make sure both solution and project are properly connected to 'http://rufc-devbuild.cneu.cnwk:8080/tfs/sed'.");
+		}
+
+		private void CheckSolutionConfigurationPlatforms()
+		{
+			var index = m_lines.IndexOf("\tGlobalSection(SolutionConfigurationPlatforms) = preSolution");
+			if (index == -1)
+				throw new FailedCheckException(@"Something went wrong while trying to find platforms configuration within a solution file.");
+
+			if (m_lines[index + 1] == "\t\tDebug|Any CPU = Debug|Any CPU"
+				&& m_lines[index + 2] == "\t\tRelease|Any CPU = Release|Any CPU")
+			{
+				return;
+			}
+
+			throw new FailedCheckException("We agreed solution configuration should only have one platform defined with the default name 'Any CPU'.");
+		}
+
+		private void CheckProjectConfigurationPlatforms()
+		{
+			foreach (var project in m_projects)
+			{
+				var debugPlatform = CheckProjectConfigurationPlatform(project.Key, "Debug");
+				var releasePlatform = CheckProjectConfigurationPlatform(project.Key, "Release");
+
+				if (debugPlatform != releasePlatform)
+				{
+					throw new FailedCheckException(
+						@"Something looks wrong with project '{0}' configuration within primary solution.
+It seems specifying different platforms for 'Debug' and 'Release' configurations: '{1}' and '{2}'.",
+						project.Value,
+						debugPlatform,
+						releasePlatform);
+				}
+			}
+		}
+
+		private string CheckProjectConfigurationPlatform(string projectUid, string configuration)
+		{
+			var prefix1 = String.Format("\t\t{0}.{1}|Any CPU.ActiveCfg", projectUid, configuration);
+			var prefix2 = String.Format("\t\t{0}.{1}|Any CPU.Build.0", projectUid, configuration);
+
+			var line1 = m_lines.FirstOrDefault(line => line.StartsWith(prefix1));
+			var line2 = m_lines.FirstOrDefault(line => line.StartsWith(prefix2));
+
+			if (line1 == null || line2 == null)
+			{
+				throw new FailedCheckException(
+					@"Something looks wrong with project '{0}' configuration within primary solution.
+Please make sure the project is configured to be built for '{1}' solution configuration in 'Configuration Manager' dialog.",
+					m_projects[projectUid],
+					configuration);
+			}
+
+			var value1 = line1.Split(new[] { " = " }, StringSplitOptions.None)[1];
+			var value2 = line2.Split(new[] { " = " }, StringSplitOptions.None)[1];
+
+			var parts1 = value1.Split('|');
+			var parts2 = value2.Split('|');
+
+			if (parts1[0] != configuration || parts2[0] != configuration)
+			{
+				throw new FailedCheckException(
+					@"Something looks wrong with project '{0}' configuration within primary solution.
+Please make sure the project is using '{1}' configuration for '{1}' solution configuration in 'Configuration Manager' dialog.",
+					m_projects[projectUid],
+					configuration);
+			}
+
+			if (parts1[1] != parts2[1])
+			{
+				throw new FailedCheckException(
+					@"Something looks wrong with project '{0}' configuration within primary solution.
+For some reason it specifies different platforms for 'Build' and 'Active configuration' properties under '{1}' configuration: '{2}' and '{3}'.",
+					m_projects[projectUid],
+					configuration,
+					parts1[1],
+					parts2[1]);
+			}
+
+			return parts1[1];
 		}
 	}
 }
--- CCNet.Build.CheckProject/Issues/ProjectProperties/CheckProjectPlatforms.cs
+++ CCNet.Build.CheckProject/Issues/ProjectProperties/CheckProjectPlatforms.cs
@@ -1,4 +1,7 @@
-﻿namespace CCNet.Build.CheckProject
+﻿using System;
+using System.Linq;
+
+namespace CCNet.Build.CheckProject
 {
 	public class CheckProjectPlatforms : IChecker
 	{
@@ -6,16 +9,43 @@
 		{
 			var project = context.ProjectDocument.Result;
 			var platforms = project.GetUsedPlatforms();
-			platforms.Remove("AnyCPU");
-			platforms.Remove("x86");
 
-			if (platforms.Count == 0)
-				return;
+			if (platforms.Count != 1)
+			{
+				throw new FailedCheckException(
+					"We agreed project should only have one platform defined, but {0} platforms were found: {1}.",
+					platforms.Count,
+					String.Join(", ", platforms.Select(name => "'" + name + "'")));
+			}
 
-			throw new FailedCheckException(
-				@"Found unknown configuration '{0}'.
+			var platform = platforms[0];
+
+			if (platform != "AnyCPU"
+				&& platform != "x64"
+				&& platform != "x86")
+			{
+				throw new FailedCheckException(
+					@"Found unknown configuration '{0}'.
 Please use standard platforms only, the most common and recommended one is 'Any CPU'.",
-				platforms[0]);
+					platform);
+			}
+
+			var debug = context.ProjectDebugProperties.Result;
+			debug.CheckOptional("PlatformTarget", platform);
+
+			var release = context.ProjectReleaseProperties.Result;
+			release.CheckOptional("PlatformTarget", platform);
+
+			var condition = project.GetDefaultPlatform();
+			if (condition != platform)
+			{
+				throw new FailedCheckException(
+					@"According to the project configuration, the default value for the '$(Platform)' build variable should be set to '{0}', but now it's '{1}'.
+This might happen, especially if project platform was changed after the project was initially created with another platform (e.g. from AnyCPU to x64).
+You manually edit the project file, by locating the tag <Platform Condition="" '$(Platform)' == '' "">... and changing its value.",
+					platform,
+					condition);
+			}
 		}
 	}
 }
